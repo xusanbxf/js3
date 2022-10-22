cdb80b39040b449c8d5282c9df47f6d4e6a60d8d
xusan <xusan007>
2022-10-22 11:17:04 +0500

    js3

---
+++ main.js
@@ -0,0 +1,14 @@
+let login = prompt('login kriting')
+let key = prompt('parolni kiriting')
+let keyy = ('123')
+let ans = ('husan07')
+if (ans == login) {
+    alert('hello boss'); 
+    if (keyy == key) {
+        alert('maroqli ish')
+    }
+}else(
+    alert('hello guest')
+)
+   
+    
